637f94674e24732d8ffc7f5d742cf7e4301828da
Tu Bui <buicaotu@gmail.com>
2024-11-18 12:02:59 +1100

    add copilot chat

--- init.lua
+++ init.lua
@@ -49,7 +49,23 @@ require("lazy").setup({
    }
   },
   "lukas-reineke/lsp-format.nvim",
+
+  -- copilot
   "github/copilot.vim",
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    branch = "canary",
+    dependencies = {
+      { "github/copilot.vim" },
+      { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+    },
+    build = "make tiktoken", -- Only on MacOS or Linux
+    opts = {
+      -- See Configuration section for options
+    },
+    -- See Commands section for default commands if you want to lazy load on them
+  },
+
   {
    "nvim-treesitter/nvim-treesitter",
    build = ':TSUpdate'
