b0dda193f9c6846ac26594769cb3bf5ca7cd37e7
Tu Bui <buicaotu@gmail.com>
2025-03-18 21:17:56 +1100

    add file path retrieval function to fzf provider options

--- init.lua
+++ init.lua
@@ -106,6 +106,22 @@ require("lazy").setup({
       -- },
       file_selector = {
         provider = "fzf",
+        provider_opts = {
+          ---@param params avante.file_selector.opts.IGetFilepathsParams
+          get_filepaths = function(params)
+            local cwd = params.cwd ---@type string
+            local selected_filepaths = params.selected_filepaths ---@type string[]
+            local cmd = string.format("fd --base-directory '%s' --hidden", vim.fn.fnameescape(cwd))
+            local output = vim.fn.system(cmd)
+            local filepaths = vim.split(output, "\n", { trimempty = true })
+            return vim
+              .iter(filepaths)
+              :filter(function(filepath)
+                return not vim.tbl_contains(selected_filepaths, filepath)
+              end)
+              :totable()
+          end
+        }
       },
       mappings = {
         ask = "<leader>aa", -- ask
