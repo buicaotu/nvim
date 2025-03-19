2893d08682bdaddf4c401a5042e6423efa61c818
Tu Bui <buicaotu@gmail.com>
2025-03-19 23:55:55 +1100

    Remove lsp-zero & luasnip

--- init.lua
+++ init.lua
@@ -12,22 +12,14 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-  {
-   "VonHeikemen/lsp-zero.nvim",
-   branch = 'v3.x',
-   dependencies = {
-     --- Uncomment these if you want to manage LSP servers from neovim
-     { 'williamboman/mason.nvim' },
-     { 'williamboman/mason-lspconfig.nvim' },
+  -- LSP Support
+  { 'neovim/nvim-lspconfig' },
+  { 'williamboman/mason.nvim' },
+  { 'williamboman/mason-lspconfig.nvim' },
+  -- Autocompletion
+  { 'hrsh7th/nvim-cmp' },
+  { 'hrsh7th/cmp-nvim-lsp' },
 
-     -- LSP Support
-     { 'neovim/nvim-lspconfig' },
-     -- Autocompletion
-     { 'hrsh7th/nvim-cmp' },
-     { 'hrsh7th/cmp-nvim-lsp' },
-     { 'L3MON4D3/LuaSnip' },
-   }
-  },
   "lukas-reineke/lsp-format.nvim",
 
   -- copilot
