2cc6298c1560bebd9e74d692f7dab6ac95551b6a
Tu Bui <buicaotu@gmail.com>
2025-03-21 23:27:07 +1100

    treesitter new mapping update

--- lua/personal/treesitter.lua
+++ lua/personal/treesitter.lua
@@ -156,19 +156,15 @@ configs.setup({
 local ts_repeat_move_status, ts_repeat_move = pcall(require, "nvim-treesitter.textobjects.repeatable_move")
 if ts_repeat_move_status then
   -- Repeat movement with ; and ,
-  -- ensure ; goes forward and , goes backward regardless of the last direction
-  -- vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move_next)
-  -- vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_previous)
-
   -- vim way: ; goes to the direction you were moving.
   vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move)
   vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_opposite)
 
-  -- Optionally, make builtin f, F, t, T also repeatable with ; and ,
-  vim.keymap.set({ "n", "x", "o" }, "f", ts_repeat_move.builtin_f)
-  vim.keymap.set({ "n", "x", "o" }, "F", ts_repeat_move.builtin_F)
-  vim.keymap.set({ "n", "x", "o" }, "t", ts_repeat_move.builtin_t)
-  vim.keymap.set({ "n", "x", "o" }, "T", ts_repeat_move.builtin_T)
+  -- make builtin f, F, t, T also repeatable with ; and ,
+  vim.keymap.set({ "n", "x", "o" }, "f", ts_repeat_move.builtin_f_expr, { expr = true })
+  vim.keymap.set({ "n", "x", "o" }, "F", ts_repeat_move.builtin_F_expr, { expr = true })
+  vim.keymap.set({ "n", "x", "o" }, "t", ts_repeat_move.builtin_t_expr, { expr = true })
+  vim.keymap.set({ "n", "x", "o" }, "T", ts_repeat_move.builtin_T_expr, { expr = true })
 else
   vim.api.nvim_err_writeln("[Error] " .. "cannot find nvim-treesitter.textobjects.repeatable_move")
 end
