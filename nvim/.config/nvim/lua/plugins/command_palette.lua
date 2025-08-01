94b147df38dca5e804a8e6ec51794560173f8511
Tu Bui <buicaotu@gmail.com>
2025-08-01 15:01:46 +1000

    alt-p

--- nvim/.config/nvim/lua/plugins/command_palette.lua
+++ nvim/.config/nvim/lua/plugins/command_palette.lua
@@ -3,7 +3,8 @@ return {
   lazy = false,
   dir = vim.fn.stdpath("config") .. "/lua/personal",
   keys = {
-    { "<Char-0xAC>", function() require("personal.command_palette").open_palette() end, desc = "Command Palette" }
+    { "<Char-0xAC>", function() require("personal.command_palette").open_palette() end, desc = "Command Palette" },
+    { "<A-p>", function() require("personal.command_palette").open_palette() end, desc = "Command Palette Alt-P" }
   },
   config = function()
     require("personal.command_palette").setup()
